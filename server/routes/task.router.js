bf4b5856df76437a1d42240169aa57b31e858f7f
Sebholmes <sebastian.holmes2001@gmail.com>
2023-10-08 17:09:14 +0300

    fixing file uploading on server-side 2

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,12 +3,16 @@ import multer from "multer";
 const router = Router();
 
 import taskController from "../controllers/tasks.controller.js";
-const upload = multer({ dest: "uploads/" });
+import path from "path";
+
+const uploadPath = path.join(__dirname, "../uploads");
+
+const upload = multer({ dest: uploadPath });
 
 router.get("/", taskController.getAllTasks);
 router.post("/", taskController.postTask);
 router.put("/description", taskController.addDescription);
 router.put("/status", taskController.changeStatus);
-// router.put("/file", upload.single("file"), taskController.uploadFile);
+router.put("/file", upload.single("file"), taskController.uploadFile);
 
 export default router;
