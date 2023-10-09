2457e46d3bf7eb0ea3acb47a5d09564063222271
Sebholmes <sebastian.holmes2001@gmail.com>
2023-10-09 16:43:51 +0300

    fixed server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import projectsRouter from "./routes/project.router.js";
 import tasksRouter from "./routes/task.router.js";
+import subtasksRouter from "./routes/subtask.router.js";
 import cors from "cors";
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(express.json());
 
 app.use("/api/v1/projects", projectsRouter);
 app.use("/api/v1/tasks", tasksRouter);
-
+app.use("/api/v1/subtasks", subtasksRouter);
 app.listen(process.env.PORT, () =>
   console.log("Server is running on port 5000")
 );
--- server/routes/subtask.router.js
+++ server/routes/subtask.router.js
@@ -6,3 +6,5 @@ import subtaskController from "../controllers/subtask.controller.js";
 router.get("/", subtaskController.getAllSubTasks);
 router.post("/", subtaskController.postSubTask);
 router.delete("/", subtaskController.deleteSubtask);
+
+export default router;
