96c8949586fe0df83874a2d7a47fea503886a4e9
Sebholmes <sebastian.holmes2001@gmail.com>
2023-10-11 16:15:10 +0300

    fix №28

--- server/controllers/comments.controller.js
+++ server/controllers/comments.controller.js
@@ -4,7 +4,7 @@ const commentsController = {
     try {
       const comments = req.body.comments;
       const query = `    INSERT INTO comments (task_id, comment_text, parent_id)
-      SELECT item ->> 'taskId' as task_id, item ->> 'comment' as comment_text item ->> 'parentId'::integer as parent_id
+      SELECT item ->> 'taskId' as task_id, item ->> 'comment' as comment_text, item ->> 'parentId'::integer as parent_id
       FROM unnest($1::json->'comments') as item; `;
       const { rows } = await pool.query(query, [comments]);
       res.status(200).json({ msg: "OK", data: rows });
