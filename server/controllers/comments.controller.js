0e2bc5e99836f3763716f7f2c3ce7e0598dbcf53
Sebholmes <sebastian.holmes2001@gmail.com>
2023-10-11 12:54:36 +0300

    fix №18

--- server/controllers/comments.controller.js
+++ server/controllers/comments.controller.js
@@ -7,9 +7,12 @@ const commentsController = {
       WITH RECURSIVE cte AS (
         SELECT * FROM unnest($1::json[]) as item
       ), recursive_cte AS (
-        SELECT item ->> 'taskId'::integer as task_id, item ->> 'comment' as comment_text, item ->> 'parentId'::integer as parent_id FROM cte WHERE item ->> 'parentId' IS NULL
+        SELECT COALESCE(item ->> 'taskId', '0')::integer as task_id, item ->> 'comment' as comment_text, COALESCE(item ->> 'parentId', '0')::integer as parent_id FROM cte WHERE item ->> 'parentId' IS NULL
         UNION ALL
-        SELECT cte.item ->> 'taskId'::integer, cte.item ->> 'comment', cte.item ->> 'parentId'::integer FROM cte JOIN recursive_cte ON cte.item ->> 'parentId' = recursive_cte.task_id
+        SELECT 
+        COALESCE(cte.item ->> 'taskId', '0')::integer,
+        cte.item ->> 'comment', 
+        COALESCE(cte.item ->> 'parentId', 'null')::integer FROM cte JOIN recursive_cte ON cte.item ->> 'parentId' = recursive_cte.task_id
       )
       INSERT INTO comments (task_id, comment_text, parent_id)
       SELECT task_id, comment_text, parent_id FROM recursive_cte
